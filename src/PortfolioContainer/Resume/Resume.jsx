331ab802626575c7298877d5b4333fd79e955be1
jafar <mirzaraximovjafar2001@gmail.com>
2022-07-31 18:31:47 +0500

    resume section

--- src/PortfolioContainer/Resume/Resume.jsx
+++ src/PortfolioContainer/Resume/Resume.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+
 import "./resume.css";
+
+import education from "../../img/Resume/education.svg";
+import history from "../../img/Resume/work-history.svg";
+import programming from "../../img/Resume/programming-skills.svg";
+import projects from "../../img/Resume/projects.svg";
+import interests from "../../img/Resume/interests.svg";
+
 const Resume = () => {
   return (
     <div id="Resume">
@@ -12,7 +20,32 @@ const Resume = () => {
           </div>
         </div>
         <div className="resume">
-          <div className="leftAbout"></div>
+          <div className="leftAbout">
+            <div className="icon-resume">
+              <img className="icons-resume" src={education} alt="education" />
+              <img className="icons-resume" src={history} alt="history" />
+              <img className="icons-resume" src={programming} alt="program" />
+              <img className="icons-resume" src={projects} alt="projects" />
+              <img className="icons-resume" src={interests} alt="interests" />
+            </div>
+            <div className="icon-info-resume">
+              <a className="linkResume" href="#education">
+                education
+              </a>
+              <a className="linkResume" href="#work-history">
+                work-history
+              </a>
+              <a className="linkResume" href="#programming-skills">
+                programming-skills
+              </a>
+              <a className="linkResume" href="#projects">
+                projects
+              </a>
+              <a className="linkResume" href="#interests">
+                interests
+              </a>
+            </div>
+          </div>
           <div className="rightAbout"></div>
         </div>
       </div>
